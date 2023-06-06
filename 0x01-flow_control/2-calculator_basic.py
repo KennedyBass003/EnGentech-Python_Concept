2beeeb28344fa3c4f514c0dcf6f8c715c2c1f516
Kennedy <113941485+KennedyBass003@users.noreply.github.com>
2023-06-06 08:01:49 +0100

    Create 2-calculator_basic.py

---
+++ 0x01-flow_control/2-calculator_basic.py
@@ -0,0 +1,25 @@
+def add_num(num1, num2):
+    print(num1 + num2)
+
+
+def sub_num(num1, num2):
+    print(num1 - num2)
+
+
+def mult_num(num1, num2):
+    print(num1 * num2)
+
+
+def div_num(num1, num2):
+    print(num1 / num2)
+
+
+def rem_num(num1, num2):
+    print(num1 % num2)
+
+
+add_num(2, 4)
+sub_num(7, 3)
+mult_num(2, 9)
+div_num(64, 8)
+rem_num(5, 2)
