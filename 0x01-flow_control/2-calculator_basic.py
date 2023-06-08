827e51ce6a0604e6ac5abf05f84e154e278318de
Kennedy <113941485+KennedyBass003@users.noreply.github.com>
2023-06-08 10:36:32 +0100

    Update 2-calculator_basic.py

--- 0x01-flow_control/2-calculator_basic.py
+++ 0x01-flow_control/2-calculator_basic.py
@@ -1,3 +1,5 @@
+
+
 def add_num(num1, num2):
     print(num1 + num2)
 
@@ -18,8 +20,11 @@ def rem_num(num1, num2):
     print(num1 % num2)
 
 
-add_num(2, 4)
-sub_num(7, 3)
-mult_num(2, 9)
-div_num(64, 8)
-rem_num(5, 2)
+num1=int(input("Enter a  first value: "))
+num2=int(input("Enter a  second value: "))
+
+add_num(num1, num2)
+sub_num(num1, num2)
+mult_num(num1, num
+div_num(num1, num2)
+rem_num(num1, num2)
