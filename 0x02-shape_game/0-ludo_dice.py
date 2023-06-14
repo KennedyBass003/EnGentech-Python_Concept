5799657c6cee5ca92361bfe853c3e30521c03de3
Kennedy <113941485+KennedyBass003@users.noreply.github.com>
2023-06-14 12:00:06 +0100

    Update 0-ludo_dice.py

--- 0x02-shape_game/0-ludo_dice.py
+++ 0x02-shape_game/0-ludo_dice.py
@@ -1,1 +1,16 @@
+import random
 
+print('Welcome to Ludo game')
+
+pick = {'toss','quit'}
+while True:
+  R1 = random.randint(0,7)
+  R2 = random.randint(0,7)
+  name=input("Enter toss/quit: \t")
+  if name not in pick:
+    print("Invalid input")
+  else:
+    if name == "toss":
+        print(R1, ":", R2)
+    elif name == "quit":
+        exit("Goodbye")
